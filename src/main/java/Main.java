ae96d87843bd13ee6e19a1dead6ecaff0da8469d
준화 <junwha0511@naver.com>
2021-03-28 20:55:15 +0900

    Seprated main and Refactoring
    -sperated main file
    -refactored file reading processes

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import com.preprocess.DataLoader;
+
+public class Main {
+    public static void main(String[] args){
+        DataLoader.read();
+    }
+}
--- src/main/java/com/preprocess/DataLoader.java
+++ src/main/java/com/preprocess/DataLoader.java
@@ -5,43 +5,52 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
+
+/*
+RATINGS FILE
+UserID::MovieID::Rating::Timestamp
+
+USER FILE
+UserID::Gender::Age::Occupation::Zip-code
+
+MOVIES FILE
+MovieID::Title::Genres
+ */
 
 public class DataLoader {
-    public static void main(String args[]){
+    public static void read(){
         try{
-            //File streams
-            File ratingFile = new File("./data/ratings.dat");
-            File userFile = new File("./data/users.dat");
-            File movieFile = new File("./data/movies.dat");
-
+            //File stream
+            File file;
             //File readers
-            FileReader fileReader = new FileReader(ratingFile);
-            BufferedReader buffReader = new BufferedReader(fileReader);
+            FileReader fileReader;// = new FileReader(ratingFile);
+            BufferedReader buffReader; // = new BufferedReader(fileReader);
 
-            String buffer = "";
+            String[] dirs = {"./data/movies.dat", "./data/users.dat", "./data/ratings.dat"};
+            ArrayList<ArrayList<String>> contents = new ArrayList<ArrayList<String>>(3);
 
-            //print ratings.dat
-            while((buffer = buffReader.readLine()) != null){
-                System.out.println(buffer);
+            //Data Reading
+            for(int i=0; i<3; i++){
+                file = new File(dirs[i]);
+                fileReader = new FileReader(file);
+                buffReader = new BufferedReader(fileReader);
+
+                String buffer = "";
+                while((buffer = buffReader.readLine()) != null){
+                    contents.get(i).add(buffer);
+                }
+
+                fileReader.close();
+                buffReader.close();
             }
 
-            //print users.dat
-            fileReader = new FileReader(userFile);
-            buffReader = new BufferedReader(fileReader);
-            while((buffer = buffReader.readLine()) != null){
-                System.out.println(buffer);
-            }
+            //preprocess movies
 
-            //print movies.dat
-            fileReader = new FileReader(movieFile);
-            buffReader = new BufferedReader(fileReader);
-            while((buffer = buffReader.readLine()) != null){
-                System.out.println(buffer);
-            }
+            //preprocess users
 
-            //close readers
-            fileReader.close();
-            buffReader.close();
+            //preprocess ratings
+
 
         }catch (FileNotFoundException e){
             System.out.println("[!File NOT FOUND] Please clone git again");

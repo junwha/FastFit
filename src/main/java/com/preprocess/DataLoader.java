46ebac78dc19a2241d94886ff8a3bf306f8c1f30
준화 <junwha0511@naver.com>
2021-03-28 21:55:15 +0900

    Update DataLoader
    -Initialized nested ArrayList in for loop
    -set ArrayList of movies, users and ratings as fields of DataLoader

--- src/main/java/com/preprocess/DataLoader.java
+++ src/main/java/com/preprocess/DataLoader.java
@@ -8,17 +8,21 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 /*
-RATINGS FILE
-UserID::MovieID::Rating::Timestamp
+MOVIES FILE
+MovieID::Title::Genres
 
 USER FILE
 UserID::Gender::Age::Occupation::Zip-code
 
-MOVIES FILE
-MovieID::Title::Genres
+RATINGS FILE
+UserID::MovieID::Rating::Timestamp
  */
 
 public class DataLoader {
+    public ArrayList<Movie> movies = new ArrayList<Movie>(0);
+    public ArrayList<User> users = new ArrayList<User>(0);
+    public ArrayList<Rating> ratings = new ArrayList<Rating>(0);
+
     public static void read(){
         try{
             //File stream
@@ -28,16 +32,19 @@ public class DataLoader {
             BufferedReader buffReader; // = new BufferedReader(fileReader);
 
             String[] dirs = {"./data/movies.dat", "./data/users.dat", "./data/ratings.dat"};
-            ArrayList<ArrayList<String>> contents = new ArrayList<ArrayList<String>>(3);
+            ArrayList<ArrayList<String>> contents = new ArrayList<ArrayList<String>>(0);
 
             //Data Reading
             for(int i=0; i<3; i++){
                 file = new File(dirs[i]);
                 fileReader = new FileReader(file);
                 buffReader = new BufferedReader(fileReader);
+                contents.add(new ArrayList<String>(0));
 
                 String buffer = "";
                 while((buffer = buffReader.readLine()) != null){
+                    System.out.println(buffer);
+
                     contents.get(i).add(buffer);
                 }
 
@@ -45,17 +52,29 @@ public class DataLoader {
                 buffReader.close();
             }
 
+
+
             //preprocess movies
-
-            //preprocess users
-
-            //preprocess ratings
-
+//            for(String movieData : contents.get(0)){
+//                System.out.println(movieData);
+//            }
+////
+//            //preprocess users
+//            for(String userData : contents.get(0)){
+//
+//            }
+//
+//            //preprocess ratings
+//            for(String ratingData : contents.get(0)){
+//
+//            }
 
         }catch (FileNotFoundException e){
             System.out.println("[!File NOT FOUND] Please clone git again");
         }catch (IOException e){
             System.out.println("[!File NOT CRASHED] Please clone git again");
+        }catch(Exception e){
+
         }
     }
 }

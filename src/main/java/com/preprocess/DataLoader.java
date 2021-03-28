b3c34e6d160e5fb34a7ee15d9b9e8ba83a9ee789
준화 <junwha0511@naver.com>
2021-03-28 22:42:31 +0900

    ArrayList to HashMap
    -for efficient search by ID

--- src/main/java/com/preprocess/DataLoader.java
+++ src/main/java/com/preprocess/DataLoader.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /*
 MOVIES FILE
@@ -19,35 +20,36 @@ UserID::MovieID::Rating::Timestamp
  */
 
 public class DataLoader {
-    public static ArrayList<Movie> movies = new ArrayList<Movie>(0);
-    public static ArrayList<User> users = new ArrayList<User>(0);
-    public static ArrayList<Rating> ratings = new ArrayList<Rating>(0);
+    public static HashMap<Integer, Movie> movies = new HashMap<Integer, Movie>(0);
+    public static HashMap<Integer, User> users = new HashMap<Integer, User>(0);
+
     private final static String[] occupationTable = {"other", "academic/educator", "artist",
             "clerical/admin", "college/grad student", "customer service", "doctor/health care",
-            "executive/managerial", "farmer", "homemaker", "K-12 student" , "lawyer", "programmer",
+            "executive/managerial", "farmer", "homemaker", "K-12 student", "lawyer", "programmer",
             "retired", "sales/marketing", "scientist", "self-employed", "technician/engineer",
             "tradesman/craftsman", "unemployed", "writer"};
 
-    public static void read(){
-        try{
-            //File stream
-            File file;
-            //File readers
-            FileReader fileReader;// = new FileReader(ratingFile);
-            BufferedReader buffReader; // = new BufferedReader(fileReader);
 
-            String[] dirs = {"./data/movies.dat", "./data/users.dat", "./data/ratings.dat"};
-            ArrayList<ArrayList<String[]>> contents = new ArrayList<ArrayList<String[]>>(0);
+    public static void read() {
+        //File stream
+        File file;
+        //File readers
+        FileReader fileReader;// = new FileReader(ratingFile);
+        BufferedReader buffReader; // = new BufferedReader(fileReader);
 
+        String[] dirs = {"./data/movies.dat", "./data/users.dat", "./data/ratings.dat"};
+        ArrayList<ArrayList<String[]>> contents = new ArrayList<ArrayList<String[]>>(0);
+
+        try {
             //Data Reading
-            for(int i=0; i<3; i++){
+            for (int i = 0; i < 3; i++) {
                 file = new File(dirs[i]);
                 fileReader = new FileReader(file);
                 buffReader = new BufferedReader(fileReader);
                 contents.add(new ArrayList<String[]>(0));
 
                 String buffer = "";
-                while((buffer = buffReader.readLine()) != null){
+                while ((buffer = buffReader.readLine()) != null) {
                     contents.get(i).add(buffer.split("::"));
                 }
 
@@ -56,39 +58,40 @@ public class DataLoader {
             }
 
 
-
-            //preprocess movies
-            for(String[] args : contents.get(0)){
-                movies.add(new Movie(Integer.parseInt(args[0]), args[1], args[2].split("|")));
-//                Movie test = movies.get(movies.size()-1);
-//                System.out.println(test.title);
-            }
-
-            //preprocess users
-            for(String[] args : contents.get(1)){
-                Gender g = null;
-                if(args[1].equals('M')) {
-                    g = Gender.M;
-                }else if(args[1].equals(('F'))){
-                    g = Gender.F;
-                }
-                users.add(new User(Integer.parseInt(args[0]), g,
-                        Integer.parseInt(args[2]), occupationTable[Integer.parseInt(args[3])], Integer.parseInt(args[4])));
-//                User test = users.get(users.size()-1);
-//                System.out.println(test.id);
-            }
-//
-//            //preprocess ratings
-//            for(String ratingData : contents.get(0)){
-//
-//            }
-
-        }catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println("[!File NOT FOUND] Please clone git again");
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("[!File NOT CRASHED] Please clone git again");
-        }catch(Exception e){
-
         }
+        //preprocess movies
+        for (String[] args : contents.get(0)) {
+            int id = Integer.parseInt(args[0]);
+            movies.put(id, new Movie(id, args[1], args[2].split("|")));
+//            Movie test = movies.get(id);
+//            System.out.println(test.title);
+        }
+
+        //preprocess users
+        for (String[] args : contents.get(1)) {
+            //Allocate genre(enum type)
+            Gender g = null;
+            if (args[1].equals('M')) {
+                g = Gender.M;
+            } else if (args[1].equals(('F'))) {
+                g = Gender.F;
+            }
+
+            int id = Integer.parseInt(args[0]);
+            users.put(id, new User(id, g,
+                    Integer.parseInt(args[2]), occupationTable[Integer.parseInt(args[3])], args[4]));
+//                User test = users.get(id);
+//                System.out.println(test.id);
+        }
+//
+//        //preprocess ratings
+//        for (String[] args : contents.get(2)) {
+//            //ratings.add()
+//        }
+
     }
 }

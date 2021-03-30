bdb9941233026fd10b7d51e7e06c3059c8c11c79
root <jimshim@unist.ac.kr>
2021-03-30 02:27:59 +0000

    Parse input, reject if input not 2

--- src/main/java/com/github/cse364/Main.java
+++ src/main/java/com/github/cse364/Main.java
@@ -1,8 +1,29 @@
 package com.github.cse364;
 import com.github.cse364.DataLoader;
 
+// Input format
+// java (filename) category occupation
+
+// multiple category input temporarily changed to "/"
+
 public class Main {
     public static void main(String[] args){
-        DataLoader.read();
+		
+		if(args.length != 2)
+		{
+			System.out.println("Error : Input format is 'category occupation'");
+			System.exit(0);
+		}
+
+		String[] cate = args[0].split("/");
+		String occu = args[1];
+
+//		for(int i=0; i<cate.length; i++)
+//		{
+//			System.out.println(cate[i]);
+//		}
+//		System.out.println(occu);
+        
+		DataLoader.read();
     }
 }

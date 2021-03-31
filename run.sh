d7f72ca9728e4c9ed27790f34558a18e124c8e30
junwha0511 <qbit@unist.ac.kr>
2021-03-31 23:15:53 +0900

    Update run.sh -mvn install -execute part

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/junwha0511/CSE-364-Software-Engineering.git && cd CSE-364-Software-Engineering
 # Edit below after Java/Maven project setting is done
-# mvn install
-# java -cp target/xxxxx.jar xxx.yyy.YourMain arg1 arg2
+
+mvn install
+java -cp target/cse364-project-1.0-SNAPSHOT.jar com.cse364.Main Adventure educator

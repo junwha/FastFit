5f654d34370f400c6b15dec19d8b8d60f32ffa72
준화 <qbit@unist.ac.kr>
2021-04-01 21:23:14 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git clone https://github.com/junwha0511/CSE-364-Software-Engineering.git && cd CSE-364-Software-Engineering
-# Edit below after Java/Maven project setting is done
 
 mvn install
 java -cp target/cse364-project-1.0-SNAPSHOT.jar com.cse364.Main Adventure educator

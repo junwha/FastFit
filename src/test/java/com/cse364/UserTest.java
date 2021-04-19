845242c213b90bbe6bfbb629f687e31120bb4e45
준화 <junwha0511@naver.com>
2021-04-19 22:59:16 +0900

    Create UserTest.java
    - Constructor test

---
+++ src/test/java/com/cse364/UserTest.java
@@ -0,0 +1,26 @@
+package com.cse364;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UserTest {
+    User testObject1;
+    User testObject2;
+
+    @Before
+    public void init(){
+        testObject1 = new User(1, Gender.M, 20, new Occupation(1, "Student"), "00000");
+        testObject2 = new User(1, Gender.M, 20, new Occupation(1, "Student"), "00000");
+    }
+
+    @Test
+    public void testConstructor(){
+        assertEquals(testObject1.getId(), testObject2.getId());
+        assertEquals(testObject1.getGender(), testObject2.getGender());
+        assertEquals(testObject1.getAge(), testObject2.getAge());
+        assertTrue(testObject1.getOccupation().equals(testObject2.getOccupation()));
+        assertEquals(testObject1.getZipCode(), testObject2.getZipCode());
+    }
+
+}

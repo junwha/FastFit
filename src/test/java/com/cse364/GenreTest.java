af1361d60b7d5d5f4e3220e0b2e32c1f78dca55f
준화 <junwha0511@naver.com>
2021-04-19 23:18:07 +0900

    Revert "Add Messages"
    This reverts commit 2dbb535c5cb34f49ce2e1c0dc11dea8da08d33e6.

--- src/test/java/com/cse364/GenreTest.java
+++ src/test/java/com/cse364/GenreTest.java
@@ -18,12 +18,11 @@ public class GenreTest {
 
     @Test
     public void testConstructor(){
-        assertEquals("Constructor", testedObject1.getName(), testedObject2.getName());
+        assertEquals(testedObject1.getName(), testedObject2.getName());
     }
 
     @Test
     public void testEquals(){
-        String message = "Equals";
         assertTrue(testedObject1.equals(testedObject2));
         assertFalse(testedObject1.equals(testedObject3));
     }

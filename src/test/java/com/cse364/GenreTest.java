aeb504728ea432c7fc4c94fdcf143c37bd52a0c3
준화 <junwha0511@naver.com>
2021-04-19 21:04:12 +0900

    Create GenreTest.java

---
+++ src/test/java/com/cse364/GenreTest.java
@@ -0,0 +1,29 @@
+package com.cse364;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GenreTest {
+    Genre testedObject1;
+    Genre testedObject2;
+    Genre testedObject3;
+
+    @Before
+    public void init(){
+        testedObject1 = new Genre("Horror");
+        testedObject2 = new Genre("Horror");
+        testedObject3 = new Genre("Comedy");
+    }
+
+    @Test
+    public void testEquals(){
+        assertTrue(testedObject1.equals(testedObject2));
+        assertFalse(testedObject1.equals(testedObject3));
+    }
+
+    @Test
+    public void testGetName(){
+        assertEquals(testedObject1.getName(), "Horror");
+    }
+}

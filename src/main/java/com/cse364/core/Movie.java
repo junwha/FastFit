86f13e84caee78dd8849420db48c0513ca199ae6
준화 <junwha0511@naver.com>
2021-04-20 20:05:57 +0900

    Revert "Move Movie class to core.utils"
    This reverts commit e0872759ff3ea70ecc4d8bc7a9101547a519e3e8.

--- src/main/java/com/cse364/core/Movie.java
+++ src/main/java/com/cse364/core/Movie.java
@@ -1,4 +1,4 @@
-package com.cse364.core.utils;
+package com.cse364.core;
 
 import java.util.List;
 
@@ -8,7 +8,7 @@ public class Movie {
     private List<Genre> genres;
     private String link;
 
-    public Movie(int id, String title, List<Genre> genres){
+    Movie(int id, String title, List<Genre> genres){
         this.id = id;
         this.title = title;
         this.genres = genres;

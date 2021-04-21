85026cd04b323778df2cae8d6a0e174dab16c50f
준화 <junwha0511@naver.com>
2021-04-22 01:09:04 +0900

    Refactor occupation test
    - change comparing object to comparing real value
    - remove duplicate getter tests

--- src/test/java/com/cse364/domain/OccupationTest.java
+++ src/test/java/com/cse364/domain/OccupationTest.java
@@ -10,33 +10,16 @@ import java.util.Map;
 public class OccupationTest {
     @Test
     public void testConstructor(){
-        int id = 3;
-        String name = "Admin";
-        Occupation testObject1 = new Occupation(id, name);
-        Occupation testObject2 = new Occupation(id, name);
-        assertSame(testObject1.getId(), testObject2.getId());
-        assertSame(testObject1.getName(), testObject2.getName());
-
-        testObject2 = new Occupation(0, "Others");
-        assertNotSame(testObject1.getId(), testObject2.getId());
-        assertNotSame(testObject1.getName(), testObject2.getName());
+        Occupation testObject1 = new Occupation(3, "Admin");
+        assertSame(testObject1.getId(), 3);
+        assertNotSame(testObject1.getId(), 0);
+        assertSame(testObject1.getName(), "Admin");
+        assertNotSame(testObject1.getName(), "Others");
     }
 
     @Test
     public void testEquals(){
         assertTrue(new Occupation(3, "Clerical").equals(new Occupation(3, "Admin")));
         assertFalse(new Occupation(2, "Artist").equals(new Occupation(13, "Retired")));
-    }
-
-    @Test
-    public void testGetId(){
-        assertEquals(new Occupation(3, "Clerical").getId(), 3);
-        assertNotSame(new Occupation(2, "Clerical").getId(), 1);
-    }
-
-    @Test
-    public void testGetName(){
-        assertEquals(new Occupation(3, "Clerical").getName(), "Clerical");
-        assertNotSame(new Occupation(1, "Retired").getName(), "Artist");
     }
 }

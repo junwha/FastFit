4b06ff446deadf017d7622ad8e9e7e92336c5273
준화 <junwha0511@naver.com>
2021-04-22 01:12:36 +0900

    Refactor Rating Test
    - change object comparing part to compare with real value

--- src/test/java/com/cse364/domain/RatingTest.java
+++ src/test/java/com/cse364/domain/RatingTest.java
@@ -9,11 +9,12 @@ import static org.junit.Assert.*;
 
 public class RatingTest {
     private Rating testObject1;
-    private Rating testObject2;
+    private Movie movie;
+    private User user;
 
     @Before
     public void init(){
-        Movie movie = new Movie(
+        movie = new Movie(
                 1,
                 "Toy Story",
                 new ArrayList<Genre>() {{
@@ -22,17 +23,16 @@ public class RatingTest {
                     add(new Genre("Comedy"));
                 }}
         );
-        User user = new User(1, User.Gender.M, 20, new Occupation(1, "Student"), "00000");
+        user = new User(1, User.Gender.M, 20, new Occupation(1, "Student"), "00000");
         testObject1 = new Rating(movie, user, 5, 70072);
-        testObject2 = new Rating(movie, user, 5, 70072);
     }
 
     @Test
     public void testConstructor(){
-        assertSame(testObject1.getMovie(), testObject2.getMovie());
-        assertSame(testObject1.getUser(), testObject2.getUser());
-        assertEquals(testObject1.getRating(), testObject2.getRating());
-        assertEquals(testObject1.getTimestamp(), testObject2.getTimestamp());
+        assertSame(testObject1.getMovie(), movie);
+        assertSame(testObject1.getUser(), user);
+        assertEquals(testObject1.getRating(), 5);
+        assertEquals(testObject1.getTimestamp(), 70072);
     }
 
 }

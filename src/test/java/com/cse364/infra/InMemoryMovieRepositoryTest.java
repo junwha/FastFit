50b6d511912c4ad6fa900a94d12268808cd2bde9
준화 <junwha0511@naver.com>
2021-04-22 02:08:42 +0900

    Create InMemoryMovieRepositoryTest.java

---
+++ src/test/java/com/cse364/infra/InMemoryMovieRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.cse364.infra;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import com.cse364.domain.*;
+
+public class InMemoryMovieRepositoryTest {
+    private InMemoryMovieRepository storage;
+    private List<Movie> movies = List.of(
+            new Movie(1, "A", null),
+            new Movie(2, "B", null)
+            );
+
+    @Before
+    public void init() {
+        storage = new InMemoryMovieRepository();
+    }
+
+    @Test
+    public void testAdd(){
+        storage.add(movies.get(0));
+        assertNotNull(storage.get(1));
+    }
+
+    @Test
+    public void testAll(){
+        for(Movie movie : movies){
+            storage.add(movie);
+        }
+
+        assertTrue(movies.equals(storage.all()));
+    }
+}

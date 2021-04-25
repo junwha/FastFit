5399d36a7c941076ba58255f2bc9edcfc66e3ba1
준화 <junwha0511@naver.com>
2021-04-26 00:55:24 +0900

    Implement age categorize part

--- src/main/java/com/cse364/infra/InMemoryUserRepository.java
+++ src/main/java/com/cse364/infra/InMemoryUserRepository.java
@@ -22,9 +22,18 @@ public class InMemoryUserRepository implements UserRepository {
     }
 
     public List<User> filterSimilarUser(User compareUser){
+        int age = compareUser.getAge();
+        if(age < 18){ age = 1; }
+        else if(age < 25){ age = 18; }
+        else if(age < 35){ age = 25; }
+        else if(age < 45){ age = 35; }
+        else if(age < 50){ age = 45; }
+        else if(age < 56){ age = 50; }
+        else{ age = 56; }
+
         List<User> userList = new ArrayList<>();
         for(User user: users.values()){
-            if((Math.abs(compareUser.getAge()-user.getAge()) <= 5)
+            if(age == user.getAge()
                     && (compareUser.getGender().equals(user.getGender()))
                     && (compareUser.getOccupation().equals(user.getOccupation()))){
                 userList.add(user);
--- src/test/java/com/cse364/infra/InMemoryUserRepositoryTest.java
+++ src/test/java/com/cse364/infra/InMemoryUserRepositoryTest.java
@@ -20,13 +20,13 @@ public class InMemoryUserRepositoryTest {
         InMemoryUserRepository storage = new InMemoryUserRepository();
         List<User> similarUsers = List.of(
                 new User(1, User.Gender.M, 25, new Occupation(1, "others"), "00000"),
-                new User(2, User.Gender.M, 28, new Occupation(1, "others"), "00000"),
-                new User(3, User.Gender.M, 35, new Occupation(1, "others"), "00000")
+                new User(2, User.Gender.M, 25, new Occupation(1, "others"), "00000"),
+                new User(3, User.Gender.M, 25, new Occupation(1, "others"), "00000")
         );
         List<User> notSimilarUsers = List.of(
                 new User(4, User.Gender.F, 25, new Occupation(1, "others"), "00000"),
-                new User(5, User.Gender.M, 14, new Occupation(1, "others"), "00000"),
-                new User(6, User.Gender.M, 35, new Occupation(2, "X"), "00000")
+                new User(5, User.Gender.M, 35, new Occupation(1, "others"), "00000"),
+                new User(6, User.Gender.M, 25, new Occupation(2, "X"), "00000")
         );
 
         for(User user : similarUsers){
@@ -36,7 +36,7 @@ public class InMemoryUserRepositoryTest {
             storage.add(user);
         }
 
-        assertEquals(storage.filterSimilarUser(new User(7, User.Gender.M, 30, new Occupation(1, "others"), "00000")), similarUsers);
-        assertEquals(storage.filterSimilarUser(new User(8, User.Gender.F, 10, new Occupation(3, "Y"), "00000")), List.of());
+        assertEquals(storage.filterSimilarUser(new User(7, User.Gender.M, 32, new Occupation(1, "others"), "00000")), similarUsers);
+        assertEquals(storage.filterSimilarUser(new User(8, User.Gender.F, 36, new Occupation(3, "Y"), "00000")), List.of());
     }
 }

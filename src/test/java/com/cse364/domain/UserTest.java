9157be76ab74ee8640311ab811639a4b6575edb1
Dongkyu Han <queued37@gmail.com>
2021-04-28 19:45:22 +0900

    Separate UserInfo and Gender from User class

--- src/test/java/com/cse364/domain/UserTest.java
+++ src/test/java/com/cse364/domain/UserTest.java
@@ -11,13 +11,13 @@ public class UserTest {
     @Before
     public void init(){
         occupation = new Occupation(1, "Student");
-        user1 = new User(1, User.Gender.M, 20, occupation, "00000");
+        user1 = new User(1, Gender.M, 20, occupation, "00000");
     }
 
     @Test
     public void testConstructor(){
         assertEquals(user1.getId(), 1);
-        assertEquals(user1.getGender(), User.Gender.M);
+        assertEquals(user1.getGender(), Gender.M);
         assertEquals(user1.getAge(), 20);
         assertTrue(user1.getOccupation().equals(occupation));
         assertEquals(user1.getZipCode(), "00000");

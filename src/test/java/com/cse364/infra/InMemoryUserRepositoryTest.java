3958d6c7002492c51535a31713cb0edf6833ff1e
root <jimshim@unist.ac.kr>
2021-05-02 17:16:22 +0000

    Higher coverage...

--- src/test/java/com/cse364/infra/InMemoryUserRepositoryTest.java
+++ src/test/java/com/cse364/infra/InMemoryUserRepositoryTest.java
@@ -47,4 +47,34 @@ public class InMemoryUserRepositoryTest {
         assertEquals(storage.filterSimilarUser(new User(8, Gender.F, 48, new Occupation(3, "Y"), "00000")), List.of());
         assertEquals(storage.filterSimilarUser(new User(8, Gender.F, 51, new Occupation(3, "Y"), "00000")), List.of());
     }
+    
+    @Test
+    public void testFilterSimilarUserByUserInfo() {
+        InMemoryUserRepository storage = new InMemoryUserRepository();
+        List<Integer> ageTestList = List.of(11,21,31,41,51,61);
+        List<User> ageTestUserList = List.of(new User(1, Gender.M, -1, new Occupation(1, "X"), "00000"));
+        storage.add(ageTestUserList.get(0));
+        for (Integer age : ageTestList) {
+            assertEquals(storage.filterSimilarUser(new UserInfo(null, age, null, "00000")), List.of());
+        }
+        /*
+        List<Gender> genVar = List.of(
+                null,
+                Gender.F
+        );
+        List<Integer> ageVar = List.of(
+                null.
+                -1
+        );
+        List<Occupation> occVar = List.of(
+                null,
+                new Occupation(1, "X")
+        );
+        UserInfo baseInfo1 = new UserInfo()
+        List<User> userList1 = new ArrayList<>();
+        for (int i=-1; i<65; i = i + 10) {
+            userList1.add(new User(i, Gender.M, i, new Occupation(1, "X"), "00000"));
+        }
+        */
+    }
 }

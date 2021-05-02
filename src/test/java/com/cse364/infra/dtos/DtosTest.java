f0d933c6d4b3df80719b91c49458b060c1d7170f
root <jimshim@unist.ac.kr>
2021-05-02 14:49:30 +0000

    DtosTest.java template

---
+++ src/test/java/com/cse364/infra/dtos/DtosTest.java
@@ -0,0 +1,17 @@
+package com.cse364.infra.dtos;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+public class DtosTest {
+    private List<MovieDto> movieDtos;
+    private List<RatingDto> ratingDtos;
+    private List<UserDto> userDtos;
+
+    @Before
+    public void init() {
+        
+    }
+}

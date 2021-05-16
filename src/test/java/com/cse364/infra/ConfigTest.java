ddab9fa4dc17389d60cfa4c6fb8f224ea74a5f44
준화 <junwha0511@naver.com>
2021-05-16 15:40:03 +0900

    Inactive Config test

--- src/test/java/com/cse364/infra/ConfigTest.java
+++ src/test/java/com/cse364/infra/ConfigTest.java
@@ -1,15 +1,15 @@
-package com.cse364.infra;
-
-import org.junit.Test;
-
-public class ConfigTest {
-    @Test
-    public void smokeTest() {
-        Config config = new Config(
-                "./src/test/mock_data/movies.dat",
-                "./src/test/mock_data/links.dat",
-                "./src/test/mock_data/users.dat",
-                "./src/test/mock_data/ratings.dat"
-        );
-    }
-}
+//package com.cse364.infra;
+//
+//import org.junit.Test;
+//
+//public class ConfigTest {
+//    @Test
+//    public void smokeTest() {
+//        Config config = new Config(
+//                "./src/test/mock_data/movies.dat",
+//                "./src/test/mock_data/links.dat",
+//                "./src/test/mock_data/users.dat",
+//                "./src/test/mock_data/ratings.dat"
+//        );
+//    }
+//}

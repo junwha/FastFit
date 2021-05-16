77cf01305cdba4e567a798ccf409d5fa4a705a40
준화 <junwha0511@naver.com>
2021-05-16 15:38:25 +0900

    Inactive argument controller

--- src/main/java/com/cse364/Main.java
+++ src/main/java/com/cse364/Main.java
@@ -2,19 +2,28 @@ package com.cse364;
 
 import com.cse364.infra.*;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String args[]) {
-        Config config = new Config(
-                "./data/movies.dat",
-                "./data/links.dat",
-                "./data/users.dat",
-                "./data/ratings.dat"
-        );
-        ArgumentController argumentController = new ArgumentController(
-                config.averageRatingService,
-                config.rankingService,
-                config.validationService
-        );
-        argumentController.main(args);
+//        Config config = new Config(
+//                "./data/movies.dat",
+//                "./data/links.dat",
+//                "./data/users.dat",
+//                "./data/ratings.dat"
+//        );
+//        ArgumentController argumentController = new ArgumentController(
+//                config.averageRatingService,
+//                config.rankingService,
+//                config.validationService
+//        );
+//        argumentController.main(args);
+
+
+
+        SpringApplication.run(Main.class, args);
+
     }
 }

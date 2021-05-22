d78635121959ce29974124e717ac4610112923a9
Dongkyu Han <queued37@gmail.com>
2021-05-23 04:51:45 +0900

    Use Lombok

--- src/main/java/com/cse364/api/dtos/MovieDto.java
+++ src/main/java/com/cse364/api/dtos/MovieDto.java
@@ -1,32 +1,10 @@
 package com.cse364.api.dtos;
 
-import com.cse364.domain.Genre;
-import com.cse364.domain.Movie;
+import lombok.Value;
 
-import java.util.List;
-
+@Value
 public class MovieDto {
     String title;
     String genre;
     String imdb;
-
-    public MovieDto(String title, String genre, String imdb){
-        this.title = title;
-        this.genre = genre;
-        this.imdb = imdb;
-    }
-
-
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public String getImdb() {
-        return imdb;
-    }
 }

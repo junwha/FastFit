d70979d9fad4743ccaf7c009d7499b60ff4903c2
Dongkyu Han <queued37@gmail.com>
2021-05-24 03:20:45 +0900

    Add getSimilarity for UserInfo

--- src/main/java/com/cse364/domain/UserInfo.java
+++ src/main/java/com/cse364/domain/UserInfo.java
@@ -8,4 +8,18 @@ public class UserInfo {
     int age;
     Occupation occupation;
     String zipCode;
+
+    public int getSimilarity(UserInfo another) {
+        int similarity = 0;
+        if (gender == null || another.gender == null || gender.equals(another.gender)) {
+            similarity++;
+        }
+        if (age == -1 || another.age == -1 || age == another.age) { // TODO: check age similarity
+            similarity++;
+        }
+        if (occupation == null || another.occupation == null || occupation.equals(another.occupation)) {
+            similarity++;
+        }
+        return similarity;
+    }
 }

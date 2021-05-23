67e740da11e3818e104053aa855f567d9ab9e9a0
Dongkyu Han <queued37@gmail.com>
2021-05-24 03:40:31 +0900

    Check age similarity

--- src/main/java/com/cse364/domain/UserInfo.java
+++ src/main/java/com/cse364/domain/UserInfo.java
@@ -14,12 +14,27 @@ public class UserInfo {
         if (gender == null || another.gender == null || gender.equals(another.gender)) {
             similarity++;
         }
-        if (age == -1 || another.age == -1 || age == another.age) { // TODO: check age similarity
+        if (age == -1 || another.age == -1 || areAgesSimilar(age, another.age)) {
             similarity++;
         }
         if (occupation == null || another.occupation == null || occupation.equals(another.occupation)) {
             similarity++;
         }
         return similarity;
+    }
+
+    // TODO: separate Age class
+    static boolean areAgesSimilar(int age1, int age2) {
+        return normalizeAge(age1) == normalizeAge(age2);
+    }
+
+    static int normalizeAge(int age) {
+        if (age < 18) return 1;
+        else if (age < 25) return 18;
+        else if (age < 35) return 25;
+        else if (age < 45) return 35;
+        else if (age < 50) return 45;
+        else if (age < 56) return 50;
+        else return 56;
     }
 }

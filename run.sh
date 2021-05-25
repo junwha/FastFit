adeb2ee2761865309145e91bead707125a3503e6
준화 <junwha0511@naver.com>
2021-05-25 15:57:43 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 git clone https://github.com/junwha0511/CSE-364-Software-Engineering.git && cd CSE-364-Software-Engineering
 
-mvn install
-java -cp target/cse364-project-1.0-SNAPSHOT.jar com.cse364.Main "" "" ""
-java -cp target/cse364-project-1.0-SNAPSHOT.jar com.cse364.Main "" "" "" "Adventure"
+###!!! Remove this part !!!###
+git checkout shellfile
+###!!! Remove this part !!!###
 
+mvn spring-boot:run

0c676b4e8054d6fd49b4efd0fc05fc09a45bc417
준화 <junwha0511@naver.com>
2021-05-25 23:09:35 +0900

    Remove test code fragment

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 git clone https://github.com/junwha0511/CSE-364-Software-Engineering.git && cd CSE-364-Software-Engineering
-
-###!!! Remove this part !!!###
-git checkout shellfile
-###!!! Remove this part !!!###
-
+  
 mvn spring-boot:run

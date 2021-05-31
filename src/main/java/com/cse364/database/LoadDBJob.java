e3a297bda1d360ba9ae9cd064c681c22ea546585
준화 <junwha0511@naver.com>
2021-06-01 02:42:45 +0900

    Temporary commit

---
+++ src/main/java/com/cse364/database/LoadDBJob.java
@@ -0,0 +1,74 @@
+//package com.cse364.database;
+//
+//import com.cse364.domain.User;
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.core.launch.support.RunIdIncrementer;
+//import org.springframework.batch.item.data.MongoItemWriter;
+//import org.springframework.batch.item.file.FlatFileItemReader;
+//import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+//import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+//import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.io.ClassPathResource;
+//import org.springframework.data.mongodb.MongoDbFactory;
+//import org.springframework.data.mongodb.core.MongoTemplate;
+//import org.springframework.data.mongodb.core.SimpleMongoClientDbFactory;
+//import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+//import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+//import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+//
+//import java.net.UnknownHostException;
+//
+//@EnableBatchProcessing
+//@Configuration
+//@EnableMongoRepositories
+//public class LoadDBJob {
+//
+//    @Autowired
+//    private JobBuilderFactory jobBuilderFactory;
+//    @Autowired
+//    private StepBuilderFactory stepBuilderFactory;
+//
+//
+//    @Bean
+//    public Job readCSVFile() {
+//        return jobBuilderFactory.get("readCSVFile").incrementer(new RunIdIncrementer()).start(step1())
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step1() {
+//        return stepBuilderFactory.get("step1").<User, User>chunk(10).reader(reader())
+//                .writer(writer()).build();mm
+//    }
+//
+//    @Bean
+//    public FlatFileItemReader<User> reader() {
+//        FlatFileItemReader<User> reader = new FlatFileItemReader<>();
+//        reader.setResource(new ClassPathResource("sample-data.csv"));
+//        reader.setLineMapper(new DefaultLineMapper<User>() {{
+//            setLineTokenizer(new DelimitedLineTokenizer() {{
+//                setNames(new String[]{"id", "name"});
+//
+//            }});
+//            setFieldSetMapper(new BeanWrapperFieldSetMapper<User>() {{
+//                setTargetType(User.class);
+//            }});
+//        }});
+//        return reader;
+//    }
+//
+//    @Bean
+//    public MongoItemWriter<User> writer() {
+//        MongoItemWriter<User> writer = new MongoItemWriter<User>();
+//        writer.setTemplate(mongoTemplate);
+//        writer.setCollection("domain");
+//        return writer;
+//    }
+//}

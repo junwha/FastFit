ca7776a3df00f19fd28b3887a4350f9dbc9720e2
준화 <junwha0511@naver.com>
2021-06-01 15:20:19 +0900

    Make entity classes

---
+++ src/main/java/com/cse364/database/UserEntity.java
@@ -0,0 +1,12 @@
+package com.cse364.database;
+
+import lombok.Data;
+
+@Data
+public class UserEntity {
+    String id;
+    String gender;
+    String age;
+    String occupation;
+    String code;
+}

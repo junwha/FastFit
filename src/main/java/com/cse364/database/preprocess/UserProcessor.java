09cb75e77b699dc433afeb93baa4445f3a978750
준화 <junwha0511@naver.com>
2021-06-01 17:36:05 +0900

    Implement UserProcessor

---
+++ src/main/java/com/cse364/database/preprocess/UserProcessor.java
@@ -0,0 +1,27 @@
+package com.cse364.database.preprocess;
+
+import com.cse364.database.dtos.MovieDto;
+import com.cse364.database.dtos.UserDto;
+import com.cse364.domain.Gender;
+import com.cse364.domain.Genre;
+import com.cse364.domain.Movie;
+
+
+import com.cse364.domain.User;
+import com.cse364.infra.Config;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserProcessor implements ItemProcessor<UserDto, User> {
+    @Autowired
+    Config config;
+
+    @Override
+    public User process(UserDto item) {
+        return new User(item.getId(),
+                item.getGender()=="M"? Gender.M:Gender.F,
+                item.getAge(),
+                config.occupations.searchByName(item.getOccupation()),
+                item.getCode());
+    }
+}

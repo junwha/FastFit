d1c91c6cc7ba5e8f9141f2663550793979ab502a
Han Dongkyu <queuedq@gmail.com>
2021-06-09 03:26:21 +0900

    Create Rating schema for MongoDB

---
+++ src/main/java/com/cse364/database/schemas/RatingSchema.java
@@ -0,0 +1,35 @@
+package com.cse364.database.schemas;
+
+import com.cse364.domain.Movie;
+import com.cse364.domain.User;
+import lombok.NonNull;
+import lombok.Value;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@Document(collection = "rating")
+@Value
+public class RatingSchema {
+    @Id
+    CompositeKey id;
+    @NonNull Movie movie;
+    @NonNull User user;
+    int rating;
+    int timestamp;
+
+    public RatingSchema(Movie movie, User user, int rating, int timestamp) {
+        this.id = new CompositeKey(movie.getId(), user.getId());
+        this.movie = movie;
+        this.user = user;
+        this.rating = rating;
+        this.timestamp = timestamp;
+    }
+
+    @Value
+    static class CompositeKey implements Serializable {
+        int movieId;
+        int userId;
+    }
+}

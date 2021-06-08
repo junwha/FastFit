0812db398230b4abadd33c51bd76006eaf782d11
준화 <junwha0511@naver.com>
2021-06-08 15:55:42 +0900

    Add query methods

--- src/main/java/com/cse364/database/repositories/DBMovieRepository.java
+++ src/main/java/com/cse364/database/repositories/DBMovieRepository.java
@@ -2,6 +2,14 @@ package com.cse364.database.repositories;
 
 import com.cse364.domain.Movie;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public interface DBMovieRepository extends MongoRepository<Movie, Integer> {
+    @Query("{'title': ?0)")
+    Movie get(String title);
+
+
 }
--- src/main/java/com/cse364/database/repositories/DBRatingRepository.java
+++ src/main/java/com/cse364/database/repositories/DBRatingRepository.java
@@ -1,7 +1,17 @@
 package com.cse364.database.repositories;
 
+import com.cse364.domain.Movie;
 import com.cse364.domain.Rating;
+import com.cse364.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface DBRatingRepository extends MongoRepository<Rating, String> {
+    @Query("{'movie': ?0)")
+    List<Rating> filterByMovie(Movie movie);
+
+    @Query("{'user': ?0)")
+    List<Rating> filterByUser(User user);
 }

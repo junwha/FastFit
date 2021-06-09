059c36eb2a71d839a9e09c925d1cae602c257513
준화 <junwha0511@naver.com>
2021-06-09 12:56:19 +0900

    Add non-test profile to ServerConfig

--- src/main/java/com/cse364/api/ServerConfig.java
+++ src/main/java/com/cse364/api/ServerConfig.java
@@ -7,10 +7,12 @@ import com.cse364.infra.Config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import javax.annotation.PostConstruct;
 
+@Profile("!test")
 @Configuration
 public class ServerConfig {
     Config config;

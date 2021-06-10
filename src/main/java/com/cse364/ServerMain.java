975a172182e213d0add0741868785729517363f2
준화 <junwha0511@naver.com>
2021-06-10 20:28:51 +0900

    Initialize ServerMain

--- src/main/java/com/cse364/ServerMain.java
+++ src/main/java/com/cse364/ServerMain.java
@@ -3,9 +3,10 @@ package com.cse364;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-public class ServerMain {
+public class ServerMain extends SpringBootServletInitializer {
     public static void main(String[] args){
         SpringApplication.run(ServerMain.class, args);
     }

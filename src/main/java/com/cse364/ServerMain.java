c7ac6392620f516d8ac73ec2055b416425101f56
준화 <junwha0511@naver.com>
2021-06-13 03:31:46 +0900

    Update ServerMain.java

--- src/main/java/com/cse364/ServerMain.java
+++ src/main/java/com/cse364/ServerMain.java
@@ -3,10 +3,16 @@ package com.cse364;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ServerMain extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ServerMain.class);
+    }
+
     public static void main(String[] args){
         SpringApplication.run(ServerMain.class, args);
     }

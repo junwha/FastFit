de72929c48b6ade3d7fd914d69dd62e687bc4d52
준화 <junwha0511@naver.com>
2021-06-13 03:46:11 +0900

    Fix

--- src/test/java/com/cse364/ApiIntegrationTest.java
+++ src/test/java/com/cse364/ApiIntegrationTest.java
@@ -14,6 +14,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -30,7 +31,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebAppConfiguration
+@WebMvcTest
 @ActiveProfiles("test")
 @SpringBootTest
 @RunWith(SpringRunner.class)

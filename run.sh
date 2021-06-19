fa1d9f79bbba064f713b41a45b0af9843288890b
준화 <junwha0511@naver.com>
2021-06-19 22:57:09 +0900

    Clone with token

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-git clone https://github.com/junwha0511/CSE-364-Software-Engineering.git && cd CSE-364-Software-Engineering
-  
+git clone https://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:x-oauth-basic@github.com/junwha0511/CSE-364-Software-Engineering && cd CSE-364-Software-Engineering
+git checkout shellfile # TODO: remove this part!
+
 mvn package -P init -P docker
 java -jar target/cse364-project-1.0-SNAPSHOT.jar

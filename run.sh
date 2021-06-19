cf32e2780dfd27fa3e609b5b7ac5c5f4c44d4c5d
Han Dongkyu <queuedq@gmail.com>
2021-06-20 06:02:01 +0900

    Revert shellfile related changes

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-git clone https://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:x-oauth-basic@github.com/junwha0511/CSE-364-Software-Engineering && cd CSE-364-Software-Engineering
-git checkout shellfile # TODO: remove this part!
-
-mvn package -P init
-java -jar target/cse364-project.jar
+git clone https://github.com/junwha0511/CSE-364-Software-Engineering.git && cd CSE-364-Software-Engineering
+  
+mvn package -P init -P docker
+java -jar target/cse364-project-1.0-SNAPSHOT.jar

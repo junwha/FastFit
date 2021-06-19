b87e5e81d28d3d3501f8668c5347640f9d8f76af
Han Dongkyu <queuedq@gmail.com>
2021-06-19 20:54:38 +0900

    Move Dockerfile to root and use war file in target folder

--- deploy/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git clone https://github.com/junwha0511/CSE-364-Software-Engineering.git && cd CSE-364-Software-Engineering
-  
-mvn package -P init -P docker
-java -jar target/cse364-project-1.0-SNAPSHOT.jar

399b1f66843c5c579954267fbfc56acdccc7f0c6
준화 <junwha0511@naver.com>
2021-06-20 15:09:47 +0900

    Add mongo daemon execution part

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 git clone https://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:x-oauth-basic@github.com/junwha0511/CSE-364-Software-Engineering && cd CSE-364-Software-Engineering
 git checkout shellfile # TODO: remove this part!
+mongod --fork --logpath /var/log/mongodb.log
 
 mvn package -P init
 java -jar target/cse364-project.jar
+
+

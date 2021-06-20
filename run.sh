254aef6e795275f3705965b82fa688c1d0518d9d
Dongkyu Han <queuedq@gmail.com>
2021-06-20 17:10:53 +0900

    Remove checkout in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 git clone https://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:x-oauth-basic@github.com/junwha0511/CSE-364-Software-Engineering && cd CSE-364-Software-Engineering
-git checkout dev # TODO: remove this part!
 mongod --fork --logpath /var/log/mongodb.log
 
 mvn package -P init

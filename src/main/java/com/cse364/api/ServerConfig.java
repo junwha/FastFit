e88cc67575f33053666df915c5b3faf383330a0e
준화 <junwha0511@naver.com>
2021-06-20 14:36:46 +0900

    Create bean of valid repository

--- src/main/java/com/cse364/api/ServerConfig.java
+++ src/main/java/com/cse364/api/ServerConfig.java
@@ -3,7 +3,10 @@ package com.cse364.api;
 import com.cse364.database.repositories.DBMovieRepository;
 import com.cse364.database.repositories.DBRatingRepository;
 import com.cse364.database.repositories.DBUserRepository;
+import com.cse364.database.repositories.DBValidRepository;
+import com.cse364.domain.ValidRepository;
 import com.cse364.infra.Config;
+import com.cse364.infra.DBValidRepositoryAdaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,10 +18,14 @@ import javax.annotation.PostConstruct;
 @Profile("!test")
 @Configuration
 public class ServerConfig {
-    Config config;
+    @Bean
+    public ValidRepository validRepository(DBValidRepository dbValidRepository){
+        return new DBValidRepositoryAdaptor(dbValidRepository);
+    }
 
     @Bean
     public Config configBean(DBMovieRepository movies, DBUserRepository users, DBRatingRepository ratings){
         return new Config(movies, users, ratings);
     }
+
 }
